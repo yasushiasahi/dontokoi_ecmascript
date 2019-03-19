e1e463ce3f5425a3edb1d36a1d11e72d13b9eb6e
Yasushi Asahi <asahi1600@gmail.com>
2019-03-20 02:31:44 +0900

    init

---
+++ src/main.js
@@ -0,0 +1,27 @@
+const app = document.querySelector("#app")
+const [h1Elm, ulElm, liElm, aElm] = ["h1", "ul", "li", "a"].map(el =>
+  document.createElement(el)
+)
+
+/*
+ * タイトル
+ */
+h1Elm.textContent = "どんとこいエクマスクリプト"
+app.appendChild(h1Elm)
+
+/*
+ * サイトリンク
+ */
+const siteMap = [
+  { path: "01_object-literal", name: "オブジェクトリテラル" },
+  { path: "02_arrow-function", name: "アロー関数" }
+]
+
+liElm.appendChild(aElm)
+for (const [idx, { path, name }] of siteMap.entries()) {
+  const newLiElm = liElm.cloneNode(true)
+  newLiElm.firstChild.textContent = `${idx + 1}_${name}`
+  newLiElm.firstChild.setAttribute("href", `/${path}/index.html`)
+  ulElm.appendChild(newLiElm)
+}
+app.appendChild(ulElm)
